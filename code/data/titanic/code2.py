1535e0d0681bc81153cb9ce94aadb10c830e3042
ChunYunMuXue <1851738605@qq.com>
2023-11-15 18:26:06 +0800

    titanic_final

--- code/data/titanic/code2.py
+++ code/data/titanic/code2.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt5
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import torch.onnx
-device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc. 
+device = torch.device("cuda:0")  # you can continue going on here, like cuda:1- cuda:2....etc. 
 
 import math
 
 class DiabetesDataset(Dataset):
     def __init__(self,path):
-        X = np.empty((6,))
+        X = np.empty((3,))
         Y = np.empty((1,))
         with open(path) as F:
             read = csv.DictReader(F)
@@ -30,11 +30,12 @@ class DiabetesDataset(Dataset):
                     del Line['Survived']
                 Line['Cabin'] = float(ord(Line['Cabin'][0]) - ord('A') + 1) if (len(Line['Cabin']) > 0) else 0
                 Line['Sex'] = 1 if Line['Sex'] == 'male' else 0
+                Line['Age'] = 0 if Line['Age'] == '' else Line['Age']
                 # Line['Embarked'] = 1 if Line['Embarked'] == 'C' else 2 if Line['Embarked'] == 'S' else 3
                 A = []
-                for x in Line.keys():
-                    if(Line[x] == ''):Line[x] = 0
-                    A.append(float(Line[x]))
+                A.append(float(Line['Sex']))
+                A.append(float(Line['Age']))
+                A.append(float(Line['Cabin']))
                 X = np.row_stack((X,np.array(A,dtype = float)))
         X = np.delete(X,0,0)
         Y = np.delete(Y,0,0)
@@ -59,29 +60,27 @@ train_loader = DataLoader(dataset = dataset,batch_size = 150,shuffle = True,num_
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model,self).__init__()
-        self.linear1 = torch.nn.Linear(6,4)
-        self.linear2 = torch.nn.Linear(4,2)
-        self.linear3 = torch.nn.Linear(2,1)
+        self.linear1 = torch.nn.Linear(3,2)
+        self.linear2 = torch.nn.Linear(2,1)
         self.active = torch.nn.Sigmoid() 
         self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self,x):
         x = self.active(self.linear1(x))
-        x = self.active(self.linear2(x))
-        x = self.sigmoid(self.linear3(x))
+        x = self.sigmoid(self.linear2(x))
         return x
 
 model = Model().to(device)
-criterion = torch.nn.BCELoss(reduction = 'sum')
+criterion = torch.nn.BCELoss(reduction = 'mean')
 optimizer = torch.optim.SGD(model.parameters(),lr = 0.1)
 
 if __name__ == '__main__':
     epoch_list = []
     loss_list = []
     # Training Cycle
-    for epoch in range(100):
+    for epoch in range(60):
         # Forward
-        Los = 0
+        Los = 00
         for i,data in enumerate(train_loader,0):
             inputs,labels = data
             inputs = inputs.to(device)
@@ -111,7 +110,7 @@ if __name__ == '__main__':
         # print(inputs)
         y_pred = model(inputs).item()
         print(labels.item(),y_pred)
-        if(y_pred > 0.448):
+        if(y_pred > 0.445):
             A = [int(labels.item()),1]
         else:
             A = [int(labels.item()),0]
---
+++ code/mul_class/note.md
@@ -0,0 +1,12 @@
+### notes
+
+PIL IMAGES: $Z ^ {28*28},pixel \in {0,...,255}$
+
+到
+
+PyTorch Tensor: $R ^ {1 * 28 * 28},pixle \in [0,1]$
+
+单通道图像
+三通道图像 
+
+![Alt text](image.png)
